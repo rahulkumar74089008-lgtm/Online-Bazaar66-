32775c5f4c04db2eabed255430b215d75d60dd81
rahulkumar74089008-lgtm <rahulkumar74089008@gmail.com>
2026-06-25 00:39:06 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAsTYWehPz9QGpK9hIjNY_TBY456rzcKoA",
-  authDomain: "onlinebazaar66-96e7b.firebaseapp.com",
-  projectId: "onlinebazaar66-96e7b",
-  storageBucket: "onlinebazaar66-96e7b.firebasestorage.app",
+  authDomain: "onlinebazaar66.firebaseapp.com",
+  projectId: "onlinebazaar66",
+  storageBucket: "onlinebazaar66.firebasestorage.app",
   messagingSenderId: "741329003167",
   appId: "1:741329003167:web:b42b8937d5152083b6f8aa"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
